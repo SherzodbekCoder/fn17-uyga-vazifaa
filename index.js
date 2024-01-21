99bab48eee039c504e08e0def376217d7f18d701
asosiymenu007 <asosiymenu007@gmail.com>
2024-01-21 16:52:07 +0500

    Javascript misollar

---
+++ index.js
@@ -0,0 +1,72 @@
+//1-misol
+// 1 dan 100 gacha bo'lgan sonlarni ekranga chiqaruvchi algoritm yozing
+
+/**
+ *
+* for (let num = 1; num <= 100; num++) {
+*   console.log(num);
+* }
+*/
+
+// let n = prompt("1 dan n gacha bo'lgan sonlarni chiqarish uchun n sonini kiriting:");
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+// }
+
+
+//2-misol
+// 1 dan n gacha bolgan sonlarni ekranga chiqaruvchi algoritm yozing
+
+/**
+ * 
+ * let son = +prompt("1 dan sz hohlagan songacha bolgan sonlarni chiqarish uchun bror bir son kiriting:");
+
+ * for (let counter = 0; counter <= son; counter++) {
+    console.log(counter);
+    
+}
+ */
+
+
+//3-misol
+//1 dan n gacha bolgan sonlar yigindisini hisoblovchi algoritm yozing
+
+// let sum = +prompt("Biror bir son kiriting:");
+// let res = 0;
+
+// for (let counter = 1; counter <= sum; counter++) {
+//     res += counter;
+
+// }
+// console.log(`1 dan n gacha bolgan sonlar yig'indisi ${res} ga teng:`);
+
+
+
+//4-misol
+//1 dan n gacha bolgan barcha toq sonlar yig'indisi hisoblovchi algoritm yozing 
+
+// let sum = +prompt("Biror bir son kiriting:");
+// let res = 0;
+
+// for (let counter = 0; counter <= sum; counter += 2) {
+//     res += counter;
+
+// }
+// console.log(`1 dan n gacha bolgan barcha toq sonlar yig'indisi ${res} ga teng:`);
+
+
+// 5-misol
+//a dan b gacha barcha juft sonlar kopaytmasini topuvchi algoritm yozing
+
+
+// let a = +prompt("1-chi sonni kiriting: ");
+// let b = +prompt("2-chi sonni kiriting: ");
+// let counter = 1;
+// for (let son = a; son <= b; son++) {
+//     if (son % 2 === 0) {
+//         counter *= son;
+//     }
+// }
+// console.log(`${a} dan ${b} gacha barcha juft sonlar kopaytmasi ${counter} ga teng:`);
+
+
